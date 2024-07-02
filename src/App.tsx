fa08336b796c0ed759e3e79eb8d1727d12e03f82
Barış Kahraman <baris-kahra@hotmail.com>
2024-07-03 02:06:21 +0300

    test: Verify data retrieval functionality from Supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
+import { Container } from "@mui/material"
 import Header from "./components/Header"
+import Column from "./components/board/Column"
+import NewColumn from "./components/board/NewColumn"
+import supabase from "./utils/supabase"
+import { useEffect, useState } from "react"
 
 function App() {
+  const [data , setData] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const {data, error} = await supabase.from('columns').select(`
+        id,
+        column_name,
+        tasks (title)
+        `)
+      if(!error){
+        setData(data);
+      }
+    }
+
+    getData()
+  }, [])
+
+  console.log(data);
 
   return (
     <>
       <Header />
+      <Container sx={{display:"flex",marginTop:"24px", gap:"24px"}}>
+        <Column />
+        <Column />
+        <Column />
+        <Column />
+        <NewColumn />
+      </Container>
       
     </>
   )
