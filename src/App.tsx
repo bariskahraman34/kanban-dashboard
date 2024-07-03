a1416a139bcd1c88acb06811d0e6263f36f8cbc7
Barış Kahraman <baris-kahra@hotmail.com>
2024-07-03 22:03:27 +0300

    feat: Add isLoading state and loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,65 @@
-import { Container } from "@mui/material"
-import Header from "./components/Header"
-import Column from "./components/board/Column"
-import NewColumn from "./components/board/NewColumn"
-import useAppContext from "./context/context"
+import { Container, Typography, CircularProgress } from '@mui/material';
+import Header from './components/Header';
+import useAppContext from './context/context';
+import Column from './components/board/Column';
+import NewColumn from './components/board/NewColumn';
+import AddNewButton from './components/buttons/AddNewButton';
 
 function App() {
-
-  const {columns} = useAppContext();
+  const { columns, isLoading } = useAppContext();
 
   return (
     <>
       <Header />
-      <Container sx={{display:"flex",marginTop:"24px", gap:"24px"}}>
-        {columns.map((col,index) => (
-          <Column key={index} column={col} tasks={col.tasks} />
-        ))}
-        <NewColumn />
-      </Container>
-      
+      {isLoading ? 
+      (
+        <Container
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            gap: '20px',
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%,-50%)',
+          }}
+        >
+          <CircularProgress />
+        </Container>
+      ) 
+      : 
+      columns.length > 0 ? 
+      (
+        <Container sx={{ display: 'flex', marginTop: '24px', gap: '24px' }}>
+          {columns.map((col, index) => (
+            <Column key={index} column={col} tasks={col.tasks} />
+          ))}
+          <NewColumn />
+        </Container>
+      ) 
+      : 
+      (
+        <Container
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            gap: '20px',
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%,-50%)',
+          }}
+        >
+          <Typography>This board is empty. Create a new column to get started.</Typography>
+          <AddNewButton text="+Add New Column" />
+        </Container>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -10,20 +10,22 @@ type AppContextProviderProps = {
 export const AppContextProvider : React.FC<AppContextProviderProps> = ({children}) => {
 
   const [columns , setColumns] = useState<ColumnType[]>([]);
+  const [isLoading , setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     async function getData() {
       const {data:column , error:columnError} = await supabase.from('columns').select(`id,column_name,tasks(column_id,title,description,subtasks(subtask,status))`);
       if(!columnError){
         setColumns(column);
       }
+      setIsLoading(false);
     }
-
     getData()
   }, [])
 
   return(
-    <AppContext.Provider value={{columns}}>
+    <AppContext.Provider value={{columns,isLoading}}>
       {children}
     </AppContext.Provider>
   )
