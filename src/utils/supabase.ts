3d8ba4f949cefd6b4a6ddcab257f9cbeec23b8c2
Barış Kahraman <baris-kahra@hotmail.com>
2024-07-07 20:56:05 +0300

    chore: Moved Supabase key and URL from .env to supabase file.

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://jghzvgdtpkjghygqyifm.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpnaHp2Z2R0cGtqZ2h5Z3F5aWZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk5NTEyNjIsImV4cCI6MjAzNTUyNzI2Mn0.VUXMegCEGlkOKe065mPXrO6J0pP_ClVkQ2p3yUGXrBU";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase
